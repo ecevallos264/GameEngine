2e7f2ddeaca2b182c6aa5e4bc03c41824d4594c1
eceva <ecevallos264@gmail.com>
2024-11-17 21:25:34 -0500

    square

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <filesystem>
 #include "shader-compiler.h"
+#include "stb_image.h"
 
 #define WINDOW_HEIGHT 600
 #define WINDOW_WIDTH 800
@@ -81,8 +82,73 @@ int main() {
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))); // TexCoord
     glEnableVertexAttribArray(2);
 
+    // ******
+    unsigned int texture1, texture2;
+
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    std::filesystem::path containerPath = contentRoot / "textures/container.jpg";
+    std::filesystem::path awesomefacePath = contentRoot / "textures/awesomeface.png";
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned char *data = stbi_load(containerPath.string().c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+
+    data = stbi_load(awesomefacePath.string().c_str(), &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    shader.use();
+
+    glUniform1i(glGetUniformLocation(shader.ID, "texture1"), 0);
+
+    shader.setInt("texture2", 1);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
         renderGame(window, shader, VAO, VBO);
         glfwSwapBuffers(window);
         glfwPollEvents();
--- shaders/shader.frag
+++ shaders/shader.frag
@@ -4,9 +4,11 @@ out vec4 FragColor;
 in vec3 vertexColor;
 in vec2 textureCoordinates;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 void main()
 {
-    FragColor = vec4(vertexColor, 1.0);
-//    FragColor = texture(ourTexture, textureCoordinates);
+//    FragColor = vec4(vertexColor, 1.0);
+    FragColor = mix(texture(texture1, textureCoordinates), texture(texture2, textureCoordinates), 0.2);
 }
